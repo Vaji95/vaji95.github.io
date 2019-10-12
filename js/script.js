2de9be5fa10b57fd83acabe3fae65452f9fbc5f7
Vajeeran Logeswaran <vajeeran.logeswaran@advis.ch>
2019-10-12 03:34:49 +0200

    Scrolldown added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
     $('#slides').superslides({
         animation: 'fade',
-        play: 3000,
+        play: 5000,
         pagination: false,
 
     });
 
     var typed = new Typed(".typed",{
-        strings: ["C#", "Javascript", "React", "Microsoft Azure", "HTML", "CSS", "VUE", "Angular"],
+        strings: ["Javascript", "React", "Microsoft", "CSharp", "HTML", "Azure", "Java","Flutter","CSS", "Python","Vue","Bot", "Angular", "Web", "Node", "Next", "Wordpress"],
         typeSpeed: 70,
         loop:true,
         startDelay: 1000,
         backSpeed:70,
+        smartBackspace:true
        
     })
 });
